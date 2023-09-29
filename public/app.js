be8db2cca5972dc3c318d9123bdffd1641acb8b2
katkes <keshank1@hotmail.com>
2023-09-29 19:56:01 -0400

    Made hello world site with button

---
+++ public/app.js
@@ -0,0 +1,12 @@
+//document content loaded or not
+document.addEventListener("DOMContentLoaded", event => {
+
+    const app =firebase.app();
+    console.log(app);
+});
+
+const button = document.getElementById("tester");
+
+button.addEventListener("click", function () {
+    alert("Hello World!");
+});
