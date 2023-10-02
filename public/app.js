ddd82c1241086c2cfbe7aebb45f1023e2e6648fa
katkes <keshank1@hotmail.com>
2023-10-02 10:58:35 -0400

    Attempt 1 of configuring node.js environment for backend (app.js)
    --> Added "main" and "scripts" to package.json file

--- public/app.js
+++ public/app.js
@@ -1,12 +1,19 @@
 //document content loaded or not
+
+// import { initializeApp } from 'firebase-admin/app';
 document.addEventListener("DOMContentLoaded", event => {
 
     const app =firebase.app();
     console.log(app);
+
+    const button = document.getElementById("tester");
+
+    button.addEventListener("click", function () {
+        alert("Hello World!");
+    });
+
+
 });
 
-const button = document.getElementById("tester");
 
-button.addEventListener("click", function () {
-    alert("Hello World!");
-});
+
