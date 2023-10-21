49c947bd8ef241e1499ed93f8671b0d80537d93a
Arnab Das <arnabdas1234@hotmail.com>
2023-10-21 14:00:23 -0400

    filter for specific house properties

--- real-estate-web-application/src/topBar.js
+++ real-estate-web-application/src/topBar.js
@@ -4,14 +4,29 @@ import "./css/topBar.css";
 
 function TopBar({ setContentText }) {
   const handleButtonClick = () => {
-    // Change the content when the button is clicked
+    // Change the content when the button is clicked 
     const signUpContent = SignUp();
     setContentText(signUpContent);
   };
 
   return (
     <div className="topBar">
-        <div className="pageLogo"> <a href="public/index.html"><img src="Logo.png" alt="Urban Utopia"></img></a>   </div>
+
+        <div className="pageLogo">
+        <a href="App.js" id="LogoAnchorHome">  
+          <img src="./Logo.png" alt="Urban Utopia">
+          </img>
+        </a>
+        </div>
+
+        <div className="filterForm"> {/*This may not even be implemented. Well see*/}
+          <form id="ApiFilterSearch">
+          <label for="filter">Filter  </label>
+          <input type="text" name="filter" id="filter"></input>
+          
+          </form>
+        </div>
+        
       <button className="signUpBtn" onClick={handleButtonClick}>Sign up</button>
     </div>
   );
