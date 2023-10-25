6a39650937a3bfe09e36d6cd777683142af50ade
thermoNuke1 <ca372717@gmail.com>
2023-10-25 18:54:05 -0400

    changed the property page

--- real-estate-web-application/src/App.js
+++ real-estate-web-application/src/App.js
@@ -6,8 +6,28 @@ import Content from './Content';
 // import PropertySection from "./PropertySection.js";
 //import HomePage from "./home";
 //import LoginForm from "./LoginForm";
-import BuyPropertyPage from "./BuyPropertyPage.js";
+import RealEstateListing from "./BuyPropertyPage.js";
 
+const propertyData = {
+  type: 'Apartment',
+  address: '123 Main St, City, State',
+  price: '$500,000',
+  previewPhotos: {
+    big: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CCDDDDDDDDDD2&t=pi&w=640&h=480&sm=c',
+    small1: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CEDDDDDDDDDDC&t=pi&w=320&h=240&sm=c',
+    small2: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CEDDDDDDDDDDC&t=pi&w=320&h=240&sm=c',
+  },
+  broker: 'John Doe',
+  favorite: false,
+  features: [
+    '3 Bedrooms',
+    '2 Bathrooms',
+    '1,200 sq. ft.',
+    'Swimming Pool',
+    'Garage',
+    'Near Schools',
+  ],
+};
 
 function App() {
   const [contentText, setContentText] = useState();/*BuyPropertyPage()*/ 
@@ -19,7 +39,7 @@ function App() {
       <SideBar/>
       <TopBar setContentText={setContentText}/>
       {/* <PropertySection/> */}
-      <BuyPropertyPage img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTVIiqTmqNZ2Pu85wNJ6LktY_UuOq5Hewg2A&usqp=CAU"name="Duplex" price="100 000" country="Canada" rating="4.6"/>
+      <RealEstateListing property={propertyData} />
     </div>
   );
 }
