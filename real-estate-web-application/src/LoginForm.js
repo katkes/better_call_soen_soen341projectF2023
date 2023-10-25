f3b5f38a42ad6e2536e33f1a20a206859022f56a
Arnab Das <arnabdas1234@hotmail.com>
2023-10-25 12:11:07 -0400

    fixed LoginForm css

--- real-estate-web-application/src/LoginForm.js
+++ real-estate-web-application/src/LoginForm.js
@@ -4,8 +4,9 @@ function LoginForm(){
 
     return(
         <div className="loginForm">
-            <form action="#" method="POST" className="loginFormElement">
+            <form action="#" method="POST">
             <h2>Login</h2>
+            <div className="loginFormElement">
             <label for="email">email address:</label>
             <input type="email" name="email"></input>
             <br></br>
@@ -14,6 +15,7 @@ function LoginForm(){
             <br></br>
             <button className="loginButton">Login</button>
             {/* <a href="App.js" className="cancelLoginButtonAnchorTag"><button className="cancelLoginButton">Cancel</button></a> */}
+            </div>
             </form>
         </div>
     );
