dc0df84eae6bce94ded75e79a9a56a1720c629b7
Arnab Das <arnabdas1234@hotmail.com>
2023-10-25 15:35:08 -0400

    propertyBuyPage created

--- real-estate-web-application/src/App.js
+++ real-estate-web-application/src/App.js
@@ -4,12 +4,12 @@ import SideBar from './sideBar';
 import TopBar from './topBar';
 import Content from './Content';
 // import PropertySection from "./PropertySection.js";
-import HomePage from "./home";
-import LoginForm from "./LoginForm";
-
+//import HomePage from "./home";
+//import LoginForm from "./LoginForm";
+import BuyPropertyPage from "./BuyPropertyPage.js";
 
 function App() {
-  const [contentText, setContentText] = useState(LoginForm());
+  const [contentText, setContentText] = useState();/*BuyPropertyPage()*/ 
 
   return (
     <div className="App">
@@ -17,7 +17,7 @@ function App() {
       <SideBar/>
       <TopBar setContentText={setContentText}/>
       {/* <PropertySection/> */}
-      
+      <BuyPropertyPage img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTVIiqTmqNZ2Pu85wNJ6LktY_UuOq5Hewg2A&usqp=CAU"name="Duplex" price="100 000" country="Canada" rating="4.6"/>
     </div>
   );
 }
