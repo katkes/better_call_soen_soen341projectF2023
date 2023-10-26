3cbbd4c79c5ac35d2a5f9ede4d7e346b5cc828ca
ThermoNuke1 <ca372717@gmail.com>
2023-10-26 14:59:47 -0400

    connect

--- real-estate-web-application/src/singularCard.js
+++ real-estate-web-application/src/singularCard.js
@@ -7,14 +7,33 @@ import React from 'react'
 
 
 function SingularCard({name,price,country,rating,setContentText}){
-
+    const propertyData = {
+        type: 'Apartment',
+        address: '123 Main St, City, State',
+        price: '$500,000',
+        previewPhotos: {
+          big: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CCDDDDDDDDDD2&t=pi&w=640&h=480&sm=c',
+          small1: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CEDDDDDDDDDDC&t=pi&w=320&h=240&sm=c',
+          small2: 'https://mspublic.centris.ca/media.ashx?id=ADDD250DC71A4CEDDDDDDDDDDC&t=pi&w=320&h=240&sm=c',
+        },
+        broker: 'John Doe',
+        favorite: false,
+        features: [
+          '3 Bedrooms',
+          '2 Bathrooms',
+          '1,200 sq. ft.',
+          'Swimming Pool',
+          'Garage',
+          'Near Schools',
+        ],
+      };
     const handleClick3 = () => {
         
-        setContentText(<RealEstateListing/>);
+        setContentText(RealEstateListing(propertyData));
       };
    /* const [displayLogin,setdisplayLogin]= useState(false);*/
     return(
-        <div onclick={handleClick3} className="card">
+        <div onClick={handleClick3} className="card">
             <img src="https://i.pinimg.com/736x/a9/8a/d3/a98ad31b4947ed09d9e3e9918cf3379b.jpg" alt="fix BOZO"></img>
             <div className="cardProperties">
                 <p>Name: {name}</p>
