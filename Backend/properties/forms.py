a079038b4e7ce9ca90dee826c011d90f5e5e86d7
roburnto <58539522+roburnto@users.noreply.github.com>
2023-11-09 15:00:48 -0500

    Added attributes to properties and integrated them into templates and respective views
    added, num_of_bedroms, for_sale, num_of_bathrroms, Square-footage

--- Backend/properties/forms.py
+++ Backend/properties/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import Property, Offer
 
-#offers are stored in properties
+# offers are stored in properties
+
 
 class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
-        fields = ['name', 'price', 'country', 'rating', 'image']
+        fields = ['name', 'price', 'country', 'rating', 'image',
+                  'for_sale', 'square_footage', 'num_of_bedrooms',
+                  'num_of_bathrooms', 'type_of_property']
 
 
 class OfferForm(forms.ModelForm):
