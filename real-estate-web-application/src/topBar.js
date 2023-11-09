07616446b2b513cf209024fd83ab284e1e2b4361
ThermoNuke1 <ca372717@gmail.com>
2023-11-09 11:40:49 -0500

    username

--- real-estate-web-application/src/topBar.js
+++ real-estate-web-application/src/topBar.js
@@ -45,7 +45,7 @@ function TopBar({ setContentText }) {
         </div>
         
         {isLoggedng ? null : (
-        <ProfileButton username={"TempUserName"} setContentText={setContentText}/>
+        <ProfileButton username={sessionStorage.getItem("Username")} setContentText={setContentText}/>
       )}
       
         {isSignUpClicked ? null : (
