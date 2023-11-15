68c08177780a1f4f97d821d23aea2c337256b622
Tahsin-Islam <tahsin_i12@outlook.com>
2023-11-15 12:32:52 -0500

    Fixing home.js

--- real-estate-web-application/src/home.js
+++ real-estate-web-application/src/home.js
@@ -4,6 +4,9 @@ function HomePage(){
     return(
         <div className="homePage">
             <img className='frontPanel' src="./HomePageFrontPanel2.jpg" alt="hell"></img>
+            <p className="p1">Find your perfect home</p>
+            <p className="p2">Discover your ideal home with our real estate listings, featuring a diverse range of properties tailored to your lifestyle. 
+                Find the perfect balance of comfort and style as you explore your dream living spaces on our user-friendly website.</p>
         </div>
 
     );
