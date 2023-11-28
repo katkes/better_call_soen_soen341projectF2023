e9cf586ed9e95fabb6fc43c175131bbaf04e6ea3
Arnab Das <arnabdas1234@hotmail.com>
2023-11-27 23:36:51 -0500

    dummy components

---
+++ real-estate-web-application/src/VisitConfirmation.js
@@ -0,0 +1,13 @@
+function VisitConfirmation(){
+
+
+    return (
+        <div>
+            <h1>
+                Confirmation has been sent!
+            </h1>
+        </div>
+    );
+}
+
+export default VisitConfirmation;
