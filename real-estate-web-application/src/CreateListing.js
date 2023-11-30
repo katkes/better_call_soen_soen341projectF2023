95b172314e9839fac345a1828b66dfbfbc437ff1
Tahsin-Islam <tahsin_i12@outlook.com>
2023-11-30 18:16:14 -0500

    Making UI of CreateListing

--- real-estate-web-application/src/CreateListing.js
+++ real-estate-web-application/src/CreateListing.js
@@ -7,31 +7,44 @@ function CreateListing({setContentText}){
 
     return(
       <div className="CreateListing">
-        <label></label>
-        <input name="Price" type="number"></input>
-
-        <label></label>
-        <select name="type" type="dropmenu">
-            <option value="house">House</option>
-            <option value="appartment">Appartment</option>
-            <option value="studio">Studio</option>
-            <option value="condo">Condo</option>
-
-        </select>
-
-        <label>Size in feet squard </label>
-        <input name="size" type="number"></input>
-
-        <label>Adress</label>
-        <input name="location" type="text"></input>
-
-        <label>Number of bedrooms</label>
-        <input name="number of bedrooms" type="number"></input>
-
-        <label>Number of bathrooms </label>
-        <input name="number of bathrooms" type="number"></input>
-
-        <button>create</button>
+        <div>
+          <label>Type</label>
+          <br></br>
+          <select name="type" type="dropmenu">
+              <option value="house">House</option>
+              <option value="appartment">Apartment</option>
+              <option value="studio">Studio</option>
+              <option value="condo">Condo</option>
+          
+          </select>
+        </div>
+        <div>
+          <label>Price</label>
+          <br></br>
+          <input name="Price" type="number"></input>
+        </div>
+        <div>
+          <label>Size in feet squared </label>
+          <br></br>
+          <input name="size" type="number"></input>
+        </div>
+        <div>
+          <label>Address</label>
+          <br></br>
+          <input name="location" type="text"></input>
+        </div>
+        <div>
+          <label>Number of bedrooms</label>
+          <br></br>
+          <input name="number of bedrooms" type="number"></input>
+        </div>
+        <div>
+          <label>Number of bathrooms </label>
+          <br></br>
+          <input name="number of bathrooms" type="number"></input>
+        </div>
+        <br></br>
+        <button>Create</button>
       </div>
     );
 }
