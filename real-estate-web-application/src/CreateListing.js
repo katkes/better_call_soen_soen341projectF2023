3efa7011a136f0654fc230276a95b829770a6b20
Tahsin-Islam <tahsin_i12@outlook.com>
2023-12-17 22:24:14 -0500

    Fixed UI of CreateListing

--- real-estate-web-application/src/CreateListing.js
+++ real-estate-web-application/src/CreateListing.js
@@ -6,45 +6,48 @@ function CreateListing({setContentText}){
 
 
     return(
-      <div className="CreateListing">
-        <div>
-          <label>Type</label>
-          <br></br>
-          <select name="type" type="dropmenu">
-              <option value="house">House</option>
-              <option value="appartment">Apartment</option>
-              <option value="studio">Studio</option>
-              <option value="condo">Condo</option>
+      <div>
+        <h2>Create a Listing</h2>
+        <p>Select the <i>type of property</i>, enter the <i>price</i>, <i>size</i>, <i>address</i>, <i>number of bedrooms</i> and <i>number of bathrooms</i>.</p>
+        <div className="input-container">
+          <div className="input-group">
+            <label>Type</label>
+            <select name="type" type="dropmenu">
+                <option value="house">House</option>
+                <option value="appartment">Apartment</option>
+                <option value="studio">Studio</option>
+                <option value="condo">Condo</option>
           
-          </select>
+            </select>
+          </div>
         </div>
-        <div>
-          <label>Price</label>
-          <br></br>
-          <input name="Price" type="number"></input>
-        </div>
-        <div>
-          <label>Size in feet squared </label>
-          <br></br>
-          <input name="size" type="number"></input>
-        </div>
-        <div>
-          <label>Address</label>
-          <br></br>
-          <input name="location" type="text"></input>
-        </div>
-        <div>
-          <label>Number of bedrooms</label>
-          <br></br>
-          <input name="number of bedrooms" type="number"></input>
-        </div>
-        <div>
-          <label>Number of bathrooms </label>
-          <br></br>
-          <input name="number of bathrooms" type="number"></input>
+        <div className="input-container">
+          <div className="input-group">
+            <label>Price ($)</label>
+            <input name="Price" type="number"></input>
+          </div>
+          <div className="input-group">
+            <label>Size (ft&sup2;) </label>
+            <input name="size" type="number"></input>
+          </div>
+          <div className="input-group">
+            <label>Address</label>
+            <input name="location" type="text"></input>
+          </div>
         </div>
         <br></br>
-        <button>Create</button>
+        <div className="input-container">
+          <div className="input-group">
+            <label>Number of bedrooms</label>
+            <input name="number of bedrooms" type="number"></input>
+          </div>
+          <div className="input-group">
+            <label>Number of bathrooms </label>
+            <input name="number of bathrooms" type="number"></input>
+          </div>
+        </div>
+        <br></br>
+        <button className="calculate-button">Create</button>
       </div>
     );
 }
